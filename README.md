905201fc988590dc1e11cc43de46a1192a960e2b
Stalin <stalinct97@gmail.com>
2018-08-09 03:28:26 -0500

    Añadida Segunda Tabla Reservaciones
    Opción para ver las dos tablas

--- README.md
+++ README.md
@@ -2,12 +2,7 @@
 
 Se implementa una bdd que exporta sus datos hacia un archivo XML que es alojado en el Internet para poder ser consumido desde cualquier otra aplicacion
 
-Autores:
-
-* Mishel Centeno
-* Stalin Maza
-
-## Uso
+## Usage
 
 Start Bootstrap es una libreria open open source library de temas bootstrap. Estos temas estan controlados bajo la licencia MIT que significa que se la puede usar incluso para proyectos comerciales.
 
@@ -22,6 +17,6 @@ Este tema fue desarrollado y es mantenido por **[David Miller](http://davidmille
 
 Esta basado en el framework[Bootstrap](http://getbootstrap.com/) creado por [Mark Otto](https://twitter.com/mdo) y [Jacob Thorton](https://twitter.com/fat).
 
-## Licencia
+## Copyright and License
 
 Copyright 2013-2018 Blackrock Digital LLC. Codigo lanzado bajo la licencia [MIT](https://github.com/BlackrockDigital/startbootstrap-1-col-portfolio/blob/gh-pages/LICENSE).
--- reservaciones.php
+++ reservaciones.php
@@ -6,7 +6,7 @@ echo '<head>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
-<title>Vehiculos Tech Car</title>
+<title>Reservaciones CM</title>
 <!-- Bootstrap core CSS -->
 <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <!-- Custom styles for this template -->
@@ -17,19 +17,20 @@ echo '<head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#">Vehiculos Tech-Car</a>
+            <a class="navbar-brand" href="#">Reservaciones CM</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
+                    <li class="nav-item">
                         <a class="nav-link" href="index.php">Vehiculos
-                <span class="sr-only">(current)</span>
+                
               </a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item active">
                         <a class="nav-link" href="reservaciones.php">Reservaciones</a>
+                        <span class="sr-only">(current)</span>
                     </li>
                 </ul>
             </div>
@@ -38,42 +39,41 @@ echo '<head>
 
   <div class="container">
         <!-- Page Heading -->
-        <h1 class="my-4"  style="text-align:center;">Listado de Vehiculos</h1>
+        <h1 class="my-4"  style="text-align:center;">Listado de Reservaciones</h1>
         <div class="row">
             <?php 
-            //$url = 'https://raw.githubusercontent.com/MishelCenteno/ProyectoSOA/master/vehiculos.xml';
-            //$url = 'vehiculosL.xml';
-            $url = 'https://jersonpruebas1997.000webhostapp.com/SOA/vehiculosL.xml';
+            //$url = 'https://raw.githubusercontent.com/MishelCenteno/ProyectoSOA/master/reservacion.xml';
+            //$url = 'reservacionesL.xml';
+            $url = 'https://jersonpruebas1997.000webhostapp.com/SOA/reservacionesL.xml';
             echo "<script>console.log( 'URL ORIGEN DATOS: " . $url . "' );</script>";
             //xml = simplexml_load_file("resultados.xml");
             $xml = simplexml_load_file($url);
-            foreach ($xml->vehiculos as $carro) 
+            foreach ($xml->reservacion as $reservar) 
                 {
                     echo '<div class="col-md-7">';
                     echo '<a href="#">
-                    <img class="img-fluid rounded mb-3 mb-md-0" style="padding: 10px;" src='. '"' . $carro->url. '" alt="">
+                    <img class="img-fluid rounded mb-2 mb-md-0" style="padding: 10px;" src='. '"' . $reservar->url. '" alt="">
                 </a>';
                 echo '</div>';
                 echo '<div class="col-md-5">';
-                echo '<h3 style="text-align:center;"> ' . $carro->nombre. '</h3>';
+                echo '<h3 style="text-align:center;"> ' . $reservar->nombreCliente. '</h3>';
                 echo '<h4>Caracteristicas</h4>';
                 echo "<ul>";
-                echo "<li>" . "Marca: " .  $carro->marca . "</li>";
-                echo "<li>" . "Modelo: " .  $carro->modelo . "</li>";
-                echo "<li>" . "Año: " .  $carro->año . "</li>";
-                echo "<li>" . "Color: " .  $carro->color . "</li>";
-                echo "<li>" . "Placa: " .  $carro->placa . "</li>";
-                echo "<li>" . "Precio: " .  $carro->precio. "</li>";
+                echo "<li>" . "Tipo: " .  $reservar->tipoHabitacion . "</li>";
+                echo "<li>" . "Codigo: " .  $reservar->codigoHabitacion . "</li>";
+                echo "<li>" . "Precio: " .  $reservar->precio . "</li>";
+                echo "<li>" . "Fecha Ingreso: " .  $reservar->fechaIngreso . "</li>";
+                echo "<li>" . "Fecha Salida: " .  $reservar->fechaSalida . "</li>";
                 echo "</ul>";
-                echo "<h4>Descripcion: </h4>";
-                echo "<p>" . $carro->descripcion . "</p>";
+                echo "<h4>Comentarios: </h4>";
+                echo "<p>" . $reservar->Comentarios . "</p>";
                 echo ' </div>';
             }             
             ?>            
         </div>
     </div>
       <!-- Begin Footer -->
-    <footer class="page-footer font-small teal pt-4">
+      <footer class="page-footer font-small teal pt-4">
     <!-- Footer Text -->
     <div class="container-fluid text-center text-md-left">
       <!-- Grid row -->
